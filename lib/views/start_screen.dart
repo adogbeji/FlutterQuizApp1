ee30623ce7d2983435de6394c95b0e78994596f0
adogbeji <baka_1993@yahoo.co.uk>
2024-05-09 13:57:26 +0100

    Start building start screen

--- lib/views/start_screen.dart
+++ lib/views/start_screen.dart
@@ -5,8 +5,15 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Start Screen'),
+    return Center(
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 200,
+          ),
+        ],
+      ),
     );
   }
 }

133c4e424cfc7c513929851c58df670aed8ce3fe
adogbeji <baka_1993@yahoo.co.uk>
2024-05-26 18:14:11 +0100

    Start adding questions

---
+++ lib/models/data/questions.dart
@@ -0,0 +1,15 @@
+import 'package:quiz_app_1/models/quiz_question.dart';
+
+List<QuizQuestion> questions = [
+  QuizQuestion(
+    'What is the capital of France?', 
+    [
+      'London',
+      'Berlin',
+      'Toronto',
+      'Paris',
+      'Sydney'
+    ], 
+    'Paris',
+  ),
+];

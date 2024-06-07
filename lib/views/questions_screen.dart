fe8928f10bf6eb180f1faa81cfd8e248c2dbcf75
adogbeji <baka_1993@yahoo.co.uk>
2024-06-07 14:02:02 +0100

    Start building questions screen

--- lib/views/questions_screen.dart
+++ lib/views/questions_screen.dart
@@ -10,8 +10,24 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Questions Screen'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text(
+            'Questions Here...',
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+        ],
+      ),
     );
   }
 }

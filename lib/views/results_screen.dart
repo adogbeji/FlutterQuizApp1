fb0dda8b0f69bd0e6504d6b830d550bd64a3d6f4
adogbeji <baka_1993@yahoo.co.uk>
2024-06-23 17:11:24 +0100

    Add more code to analyse quiz questions in results screen

--- lib/views/results_screen.dart
+++ lib/views/results_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz_app_1/models/data/questions.dart';
+
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen(this.chosenAnswers, {super.key});
 
@@ -8,13 +10,26 @@ class ResultsScreen extends StatelessWidget {
   List<Map<String, Object>> getSummaryData() {
     final List<Map<String, Object>> summary = [];
 
-    for (var x = 0; x < chosenAnswers.length; x++) {}
+    for (var x = 0; x < chosenAnswers.length; x++) {
+      summary.add(
+        {
+          'question': questions[x].question,
+          'chosen_answer': chosenAnswers[x],
+          'correct_answer': questions[x].correctAnswer,
+        }
+      );
+    }
 
     return summary;
   }
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numberOfCorrectAnswers = summaryData.where((data) {
+      return data['chosen_answer'] == data['correct_answer'];
+    }).length;
+
     return const Center(
       child: Text('Results Screen'),
     );

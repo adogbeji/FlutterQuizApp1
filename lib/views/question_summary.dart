ced4f7a8d5601533bc4398f15a62566571b47d68
adogbeji <baka_1993@yahoo.co.uk>
2024-06-27 13:34:43 +0100

    Start displaying chosen answer & correct answer

--- lib/views/question_summary.dart
+++ lib/views/question_summary.dart
@@ -15,6 +15,14 @@ class QuestionSummary extends StatelessWidget {
             return Row(
               children: [
                 Text((data['question_index'] as int).toString(),),
+                Column(
+                  children: [
+                    Text(data['question'] as String),
+                    const SizedBox(height: 7,),
+                    Text(data['chosen_answer'] as String),
+                    Text(data['correct_answer'] as String),
+                  ],
+                )
               ],
             );
           }).toList(),
